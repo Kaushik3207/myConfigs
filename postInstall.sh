a18161789d15a3e5d3e54336c73206720b7aeda1
Kaushik Sharma <kaushiksharma128@gmail.com>
2026-05-14 16:24:40 +0530

    added a shell script to install basic things for my config automatically

---
+++ postInstall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo pacman -Sy code fastfetch fish neovim starship vivaldi libreoffice-still ufw gufw
+echo 'eval "$(starship init bash)"' >>~/.bashrc
+cd ~/.local/share/
+git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
+make -C ble.sh install PREFIX=~/.local
+echo 'source -- ~/.local/share/blesh/ble.sh' >>~/.bashrc
+fastfetch --gen-config
+rm ~/.config/fastfetch/config.jsonc
+cd ~/.local/share/
+git clone https://github.com/Kaushik3207/myConfigs.git
+cd myConfigs
+cp -rfu nvim/ ~/.config/
+cp starship/starship.toml ~/.config/
+cp konsole/kaushiksColourScheme.colorscheme ~/.local/share/konsole/
+mv fonts/'DepartureMono(Default)'/ fonts/d/
+mv fonts/BigTermFont/ fonts/b/
+sudo cp -rf fonts/d /usr/local/share/fonts/
+sudo cp -rf fonts/b /usr/local/share/fonts/
+fc-cache -f
+cp -rf alacritty ~/.config/
