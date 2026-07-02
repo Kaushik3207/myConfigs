1b00ab06157725e1f517afc2b505b55069d304d1
Kaushik Sharma <kaushiksharma128@gmail.com>
2026-07-02 20:04:18 +0530

    Modify postInstall.sh to include new packages and fonts
    Added installation of 'kitty' and 'alacritty' packages. Updated font installation to copy all fonts at once.

--- postInstall.sh
+++ postInstall.sh
@@ -1,13 +1,15 @@
 # required packages are listed below as zypper(openSUSE) commands but the same packages can be installed on any distribution and the rest of the script will work fine. Thank you.
 #!/bin/bash
 # installation and initial setup
-sudo zypper in git git-lfs wget curl make gcc gcc-c++ fastfetch starship neovim
+sudo zypper in git git-lfs wget curl make gcc gcc-c++ fastfetch starship neovim kitty alacritty
 echo 'eval "$(starship init bash)"' >>~/.bashrc
 cd ~/.local/share/
 git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
 make -C ble.sh install PREFIX=~/.local
 echo 'source -- ~/.local/share/blesh/ble.sh' >>~/.bashrc
 # config placement
+cp -rf alacritty ~/.config/
+cp -rf kitty ~/.config/
 fastfetch --gen-config
 rm ~/.config/fastfetch/config.jsonc
 cd ~/.local/share/
@@ -20,12 +22,8 @@ cp konsole/kaushiksColourScheme.colorscheme ~/.local/share/konsole/
 echo 'fastfetch' >>~/.bashrc
 # font installation
 # the following set of commands install all the fonts in the fonts directory system-wide
-sudo cp -rf fonts/departureMono /usr/local/share/fonts/
-sudo cp -rf fonts/bigTerm /usr/local/share/fonts/
-sudo cp -rf fonts/terminus /usr/local/share/fonts/
+sudo cp -rf fonts/. /usr/local/share/fonts/
 fc-cache -f
-cp -rf alacritty ~/.config/
-cp -rf kitty ~/.config/
 # AppImage(s) installation (user-specific)
 mkdir -p ~/opt/
-cp -rf applications/ ~/opt/
+cp -rf applications/. ~/opt/
